4d3e867b28b1c8a985723cd260e0d1188856069f
vtthuan <vtthuan89@gmail.com>
2015-08-11 00:52:13 +0200

    add event ecouteur

--- LoginNode03/server.js
+++ LoginNode03/server.js
@@ -122,3 +122,37 @@ if (require.main === module) {
     exports.shutdown = shutdown;
     exports.port = app.get('port');
 }
+
+var onEasyrtcMsg = function (connectionObj, msg, socketCallback, next) {
+    switch (msg.msgType) {
+        case "findRoom":
+            connectionObj.getApp().getRoomNames(function (err, roomNames) {
+                var notFound = roomNames.every(function (element) {
+                    connectionObj.getApp().getRoomOccupantCount(element, function (err, number) {
+                        if (number < 2) {
+                            socketCallback({ msgType: "findRoom", msgData : { "roomName"  : element, "isNew" : false } });
+                            return false;
+                        }
+                    });
+                });
+                if (notFound) {
+                    socketCallback({ msgType: "findRoom", msgData : { "roomName" : generateUUID(), "isNew" : true } });
+                }
+                    
+            });
+            next(null);
+            break;
+
+        default:
+            easyrtc.events.emitDefault("easyrtcMsg", connectionObj, msg, socketCallback, next);
+            break;
+    }
+    easyrtc.events.emitDefault("easyrtcMsg", connectionObj, msg, socketCallback, next);
+};
+easyrtc.events.on("easyrtcMsg", onEasyrtcMsg);
+
+easyrtc.setOption("roomDefaultEnable", false);
+
+function generateUUID() {
+    return Math.random().toString(36).substr(2, 9);
+};
